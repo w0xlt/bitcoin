4cef616000bdd92b5fc99b4367fa6c0e88204096
w0xlt <94266259+w0xlt@users.noreply.github.com>
2022-01-29 12:44:28 -0300

    add test for dump human-readable dumptxoutset

--- test/functional/rpc_dumptxoutset.py
+++ test/functional/rpc_dumptxoutset.py
@@ -10,6 +10,7 @@ from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error
 
 import hashlib
+import os
 from pathlib import Path
 
 
@@ -18,16 +19,12 @@ class DumptxoutsetTest(BitcoinTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 1
 
-    def run_test(self):
-        """Test a trivial usage of the dumptxoutset RPC command."""
-        node = self.nodes[0]
-        mocktime = node.getblockheader(node.getblockhash(0))['time'] + 1
-        node.setmocktime(mocktime)
-        self.generate(node, COINBASE_MATURITY)
+    def test_dump_file(self, filename, is_human_readable, expected_digest):
 
-        FILENAME = 'txoutset.dat'
-        out = node.dumptxoutset(FILENAME)
-        expected_path = Path(node.datadir) / self.chain / FILENAME
+        node = self.nodes[0]
+
+        out = node.dumptxoutset(filename, is_human_readable)
+        expected_path = Path(node.datadir) / self.chain / filename
 
         assert expected_path.is_file()
 
@@ -42,8 +39,7 @@ class DumptxoutsetTest(BitcoinTestFramework):
         with open(str(expected_path), 'rb') as f:
             digest = hashlib.sha256(f.read()).hexdigest()
             # UTXO snapshot hash should be deterministic based on mocked time.
-            assert_equal(
-                digest, '7ae82c986fa5445678d2a21453bb1c86d39e47af13da137640c2b1cf8093691c')
+            assert_equal(digest, expected_digest)
 
         assert_equal(
             out['txoutset_hash'], 'd4b614f476b99a6e569973bf1c0120d88b1a168076f8ce25691fb41dd1cef149')
@@ -51,7 +47,30 @@ class DumptxoutsetTest(BitcoinTestFramework):
 
         # Specifying a path to an existing file will fail.
         assert_raises_rpc_error(
-            -8, '{} already exists'.format(FILENAME),  node.dumptxoutset, FILENAME)
+            -8, '{} already exists'.format(filename),  node.dumptxoutset, filename)
+
+        if (is_human_readable):
+            with open(expected_path, 'r', encoding='utf-8') as f:
+                content = f.readlines()
+                assert_equal(content[0].rstrip(),
+                    "#(blockhash 6fd417acba2a8738b06fee43330c50d58e6a725046c3d843c8dd7e51d46d1ed6),txid,vout,value,coinbase,height,scriptPubKey")
+                assert_equal(content[1].rstrip(),
+                    "213ecbdfe837a2c8ffc0812da62d4de94efce8894c67e22ff658517ecf104e03,0,5000000000,1,81,76a9142b4569203694fc997e13f2c0a1383b9e16c77a0d88ac")
+
+    def run_test(self):
+        """Test a trivial usage of the dumptxoutset RPC command."""
+        node = self.nodes[0]
+        mocktime = node.getblockheader(node.getblockhash(0))['time'] + 1
+        node.setmocktime(mocktime)
+        self.generate(node, COINBASE_MATURITY)
+
+        txt_file_digest = '5bc8a9c14d1f6d89833342dcd6014bdf9ddb5f19e3741760da6d6d666971df41'
+
+        if os.name == 'nt':
+            txt_file_digest = 'dff298e77a80bb6e292526c0729d78fdef68a537c372563b5f5dbfadddabc780'
+
+        self.test_dump_file('txoutset.dat', False, '7ae82c986fa5445678d2a21453bb1c86d39e47af13da137640c2b1cf8093691c')
+        self.test_dump_file('txoutset.txt', True, txt_file_digest)
 
 if __name__ == '__main__':
     DumptxoutsetTest().main()
