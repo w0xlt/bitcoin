1bbc387cb4935fbf1608373e1ee13a98c33f23ad
w0xlt <woltx@protonmail.com>
2022-08-14 19:33:26 -0300

    script: create a new output type `OutputType::SILENT_PAYMENT`

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3537,6 +3537,7 @@ void CWallet::SetupDescriptorScriptPubKeyMans(const CExtKey& master_key)
 
     for (bool internal : {false, true}) {
         for (OutputType t : OUTPUT_TYPES) {
+            if (t == OutputType::SILENT_PAYMENT) continue;
             auto spk_manager = std::unique_ptr<DescriptorScriptPubKeyMan>(new DescriptorScriptPubKeyMan(*this));
             if (IsCrypted()) {
                 if (IsLocked()) {
