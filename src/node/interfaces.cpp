94bbc986d176244658a931037dab67de4d9fdb4a
w0xlt <woltx@protonmail.com>
2022-08-15 10:50:02 -0300

    wallet: add Silent Payment verification

--- src/node/interfaces.cpp
+++ src/node/interfaces.cpp
@@ -34,6 +34,7 @@
 #include <policy/settings.h>
 #include <primitives/block.h>
 #include <primitives/transaction.h>
+#include <pubkey.h>
 #include <rpc/protocol.h>
 #include <rpc/server.h>
 #include <shutdown.h>
@@ -601,6 +602,10 @@ public:
     bool isSilentPaymentIndexActivatedAndSynced() override
     {
         return g_silentpaymentindex ? g_silentpaymentindex->BlockUntilSyncedToCurrentChain() : false;
+    }
+    bool getSilentTransactionPubKey(const uint256& txhash, CPubKey& sum_all_inputs_pubkey) override
+    {
+        return g_silentpaymentindex->FindSilentPayment(txhash, sum_all_inputs_pubkey);
     }
     double guessVerificationProgress(const uint256& block_hash) override
     {
